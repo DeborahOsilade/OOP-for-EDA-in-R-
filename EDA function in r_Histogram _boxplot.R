b7ff086964f9291a2d1de81c278af19a1d94daf8
Deborah Osilade <64247482+deborahosilade@users.noreply.github.com>
2021-09-01 01:01:11 +0100

    Update EDA function in r_Histogram _boxplot.R

--- EDA function in r_Histogram _boxplot.R
+++ EDA function in r_Histogram _boxplot.R
@@ -2,13 +2,14 @@
 ##function that returns both ggplot histogram
 ##and boxplot in R 
 
-quick_analysis= function(pen){
+quick_analysis= function(df){
   data = students_performance
   library(ggplot2)
-  histogramm<- ggplot(data,aes(pen))+geom_histogram(fill="white",colour="black")+ggtitle("histogram of pen" )
-  boxplott<- ggplot(data,aes(pen))+geom_boxplot(fill="dark green", colour="black")+ggtitle("boxplot of pen")
-  print(histogramm)
-  print(boxplott)
+  histogramm<- ggplot(data,aes(df))+geom_histogram(fill="white",colour="black")+ggtitle("histogram of df" )
+  boxplott<- ggplot(data,aes(pen))+geom_boxplot(fill="dark green", colour="black")+ggtitle("boxplot of df")
+  library(ggpubr)
+  plot_fun<-ggarrange(histogramm,boxplott,ncol=2)
+  print(plot_fun)
 }
 
 quick_analysis(math_score)
